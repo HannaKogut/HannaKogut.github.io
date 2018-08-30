5ff8004fec1f3df59b1e7e88df525b5cf8db3d53
HannaKogut <hanna.pol94@gmail.com>
2018-08-30 16:19:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "p {\n    font-size: 13px;\n}\n\n.card {\n    box-shadow: 2px -2px 10px 0px rgba(49, 50, 50, 0.73);\n}\n\n.commentBox .form-group:first-child,\n.actionBox .form-group:first-child {\n    width: 80%;\n}\n\n.commentBox .form-group:nth-child(2),\n.actionBox .form-group:nth-child(2) {\n    width: 100%;\n}\n\n.actionBox .form-group * {\n    width: 80%;\n}\n\n.commentList {\n    padding: 0;\n    list-style: none;\n    overflow: auto;\n    text-align: justify\n}\n\n.commentList li {\n    margin: 0;\n    margin-top: 10px;\n}\n\n.commentList li>div {\n    display: table-cell;\n}\n\n.commenterImage {\n    width: 30px;\n    margin-right: 5px;\n    height: 100%;\n    float: left;\n}\n\n.commenterImage img {\n    width: 100%;\n    border-radius: 50%;\n}\n\n.commentText p,\n{\n    width: 100%;\n}\n\n.actionBox {\n    border-top: 1px dotted #bbb;\n    padding: 10px;\n}\n\ntextarea {\n    width: 90%;\n}"
+module.exports = "p {\n    font-size: 13px;\n}\n\n.header {\n    margin-left: 4%;\n    font-size: 42px;\n    font-weight: 100;\n}\n\nh1 {\n    margin-left: 4%;\n}\n\n.card {\n    box-shadow: 2px -2px 10px 0px rgba(49, 50, 50, 0.73);\n}\n\n.commentBox .form-group:first-child,\n.actionBox .form-group:first-child {\n    width: 80%;\n}\n\n.commentBox .form-group:nth-child(2),\n.actionBox .form-group:nth-child(2) {\n    width: 100%;\n}\n\n.actionBox .form-group * {\n    width: 80%;\n}\n\n.commentList {\n    padding: 0;\n    list-style: none;\n    overflow: auto;\n    text-align: justify\n}\n\n.commentList li {\n    margin: 0;\n    margin-top: 10px;\n}\n\n.commentList li>div {\n    display: table-cell;\n}\n\n.commenterImage {\n    width: 30px;\n    margin-right: 5px;\n    height: 100%;\n    float: left;\n}\n\n.commenterImage img {\n    width: 100%;\n    border-radius: 50%;\n}\n\n.commentText p,\n{\n    width: 100%;\n}\n\n.actionBox {\n    border-top: 1px dotted #bbb;\n    padding: 10px;\n}\n\ntextarea {\n    width: 80%;\n}\n\n#square {\n    width: 40px;\n    height: 40px;\n    background: rgb(192, 86, 15);\n}\n\n#square2 {\n    width: 40px;\n    height: 40px;\n    background: rgb(73, 100, 189);\n}"
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = "p {\n    font-size: 13px;\n}\n\n.card {\n    box-shadow: 2px -
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"col-md-5\">\n    <div class=\"card\" style=\"width: 30rem;\">\n        <div class=\"card-header\">\n            <h1>#Comments</h1>\n        </div>\n        <div class=\"card-body\">\n\n            <ul class=\"commentList\" *ngFor=\"let comment of comments\">\n                <li>\n                    <div class=\"commenterImage\">\n                        <img src=\"http://placekitten.com/50/50\" />\n                    </div>\n                    <div class=\"commentText\">\n                        <p class=\"\">{{comment}}</p>\n                    </div>\n                </li>\n            </ul>\n            <div class=\"commenterImage\">\n                <img src=\"http://placekitten.com/40/40\" />\n            </div>\n            <div class=\"form-group\">\n                <textarea class=\"form-control\" #comment rows=\"3\" cols=\"40\" (keydown.control.enter)=\"addComment(comment.value)\" id=\"comment\"></textarea>\n            </div>\n\n        </div>\n    </div>"
+module.exports = "<div class=\"col-md-5\">\n    <div class=\"card\" style=\"width: 32rem;\">\n        <p class=\"header\">Comments</p>\n        <div class=\"card-body\">\n\n            <ul class=\"commentList\" *ngFor=\"let comment of comments\">\n                <li>\n                    <div class=\"commenterImage\" id=\"square\">\n\n                    </div>\n                    <div class=\"commentText\">\n                        <p class=\"\">{{comment}}</p>\n                    </div>\n                </li>\n            </ul>\n            <div class=\"commenterImage\" id=\"square2\">\n\n            </div>\n            <div class=\"form-group\">\n                <textarea class=\"form-control\" [(ngModel)]=\"filterName\" #comment rows=\"3\" cols=\"40\" (keydown.control.enter)=\"addComment(comment.value)\" id=\"comment\"></textarea>\n            </div>\n\n        </div>\n    </div>"
 
 /***/ }),
 
@@ -71,32 +71,32 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 var AppCommentsComponent = /** @class */ (function () {
     function AppCommentsComponent(getItems) {
         this.getItems = getItems;
-        this.items = [];
         this.comments = [];
     }
     AppCommentsComponent.prototype.addComment = function (comment) {
         if (comment) {
-            this.comments.push(comment);
-            localStorage.setItem('comments', JSON.stringify(comment));
+            this.getItems.setComment(comment);
         }
-    };
-    AppCommentsComponent.prototype.getComments = function () {
-        // this.getItems.getItems().subscribe(item => {
-        //   this.comments = item.comments;
-        //   this.items = item;
-        //   console.log(item);
-        // });
+        this.filterName = '';
     };
     AppCommentsComponent.prototype.ngOnInit = function () {
-        if (localStorage.getItem('comments') === null) {
+        var _this = this;
+        if (localStorage.getItem('items') === null) {
             this.comments = [];
         }
         else {
-            var comments = localStorage.getItem('items');
-            var parsedData = JSON.parse(comments);
-            this.comments = parsedData;
+            this.subscription = this.getItems.getItem().subscribe(function (item) {
+                if (item.isActive === true) {
+                    _this.comments = item.comments;
+                }
+                else {
+                    _this.comments = [];
+                }
+            });
         }
-        this.getComments();
+    };
+    AppCommentsComponent.prototype.ngOnDestroy = function () {
+        this.subscription.unsubscribe();
     };
     AppCommentsComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
@@ -120,7 +120,7 @@ var AppCommentsComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "button {\n    float: right;\n}\n\n.card {\n    box-shadow: 2px -2px 10px 0px rgba(49, 50, 50, 0.73);\n}\n\nul li .itemActive {\n    border-left: 2px solid red;\n}"
+module.exports = "p {\n    margin-left: 4%;\n    font-size: 42px;\n    font-weight: 100;\n}\n\nbutton {\n    float: right;\n    width: 100px;\n    margin-left: 25px;\n}\n\n#but {\n    /* float: right; */\n    margin-left: 15px;\n    width: 120px;\n}\n\n.input-group {\n    margin-left: 19px;\n    width: 91%;\n}\n\n.card {\n    box-shadow: 2px -2px 10px 0px rgba(49, 50, 50, 0.73);\n}\n\n.itemActive {\n    border-left: 3px solid red;\n}\n\nli {\n    font-weight: bold;\n    width: 99%;\n}"
 
 /***/ }),
 
@@ -131,7 +131,7 @@ module.exports = "button {\n    float: right;\n}\n\n.card {\n    box-shadow: 2px
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"col-md-5\">\n    <div class=\"card\" style=\"width: 30rem;\">\n        <div class=\"card-header\">\n            <h1>Items1</h1>\n        </div>\n\n        <div class=\"card-body\">\n            <div class=\"input-group mb-2\">\n                <input type=\"text\" class=\"form-control\" #itemName placeholder=\"Type name here...\" aria-describedby=\"basic-addon2\">\n                <div class=\"input-group-append\">\n                    <button class=\"btn btn-primary\" type=\"button\" (click)=\"addNewItem(itemName.value)\">Add new</button>\n                </div>\n            </div>\n            <ul class=\"list-group list-group-flush\">\n                <li class=\"list-group-item\" (click)=\"selectItem(item)\" *ngFor=\"let item of items\" [ngClass]=\"item.isActive ? 'itemActive': 'itemStatic' ? 'green' : 'red'\">{{item.name}}\n                    <span class=\" badge badge-primary badge-pill \">d045415</span>\n                    <button type=\"button \" class=\"btn btn-outline-danger \" (click)=\"deleteItem(item) \">Delete</button>\n\n                </li>\n\n            </ul>\n        </div>\n    </div>\n</div>\n<!-- <div class=\"card \">\n    <div class=\"card-header \">\n        Items\n    </div>\n    <div class=\"card-body \">\n        <div class=\"input-group mb-3 \">\n            <input type=\"text \" class=\"form-control \" #itemName placeholder=\"Recipient 's username\" aria-label=\"'Type name here... \" aria-describedby=\"basic-addon2 \">\n            <div class=\"input-group-append \">\n                <button class=\"btn btn-success \" type=\"button \" (click)=\"addNewItem(itemName.value) \">Add new</button>\n            </div>\n        </div>\n        <ul class=\"list-group \" *ngFor=\"let item of items \">\n            <li class=\"list-group-item \">{{item}}</li>\n        </ul>\n    </div> [ngClass]=\" item.isSelected ? 'sensorSelected' : 'sensorStatic'\"\n</div> -->"
+module.exports = "<div class=\"col-md-5\">\n    <div class=\"card\" style=\"width: 31rem;\">\n\n        <p>Items</p>\n\n        <div class=\"input-group mb-2\">\n            <input type=\"text\" class=\"form-control\" #itemName [(ngModel)]='filterName' placeholder=\"Type name here...\" aria-describedby=\"basic-addon2\">\n            <div class=\"input-group-append\">\n                <button id=\"but\" class=\"btn btn-info\" type=\"button\" (click)=\"addNewItem(itemName.value)\">Add new</button>\n            </div>\n        </div>\n\n        <ul class=\"list-group list-group-flush\" *ngFor=\"let item of items\">\n            <li class=\"list-group-item\" (click)=\"selectItem(item)\" [ngClass]=\"item.isActive ? 'itemActive': 'itemStatic'\">{{item.name}}\n                <span class=\" badge badge-info badge-pill \">1</span>\n                <button type=\"button\" class=\"btn btn-outline-danger\" (click)=\"deleteItem(item)\">Delete</button>\n\n            </li>\n\n        </ul>\n    </div>\n</div>\n<!-- <div class=\"card \">\n    <div class=\"card-header \">\n        Items\n    </div>\n    <div class=\"card-body \">\n        <div class=\"input-group mb-3 \">\n            <input type=\"text \" class=\"form-control \" #itemName placeholder=\"Recipient 's username\" aria-label=\"'Type name here... \" aria-describedby=\"basic-addon2 \">\n            <div class=\"input-group-append \">\n                <button class=\"btn btn-success \" type=\"button \" (click)=\"addNewItem(itemName.value) \">Add new</button>\n            </div>\n        </div>\n        <ul class=\"list-group \" *ngFor=\"let item of items \">\n            <li class=\"list-group-item \">{{item}}</li>\n        </ul>\n    </div> [ngClass]=\" item.isSelected ? 'sensorSelected' : 'sensorStatic'\"\n</div> -->"
 
 /***/ }),
 
@@ -160,40 +160,48 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 
 var AppItemsComponent = /** @class */ (function () {
     function AppItemsComponent(getItems) {
+        var _this = this;
         this.getItems = getItems;
-        this.isActive = false;
         this.items = [];
+        this.subscription = this.getItems.getComment().subscribe(function (data) {
+            _this.items.forEach(function (item) {
+                if (item.isActive === true) {
+                    item.comments.push(data);
+                }
+            });
+            localStorage.setItem('items', JSON.stringify(_this.items));
+        });
     }
-    AppItemsComponent.prototype.addNewItem = function (text) {
-        if (text) {
-            var comments = localStorage.getItem('comments');
-            var itemObject = {
-                'name': text,
-                'comments': comments
-            };
-            this.items.push(itemObject);
+    AppItemsComponent.prototype.addNewItem = function (name) {
+        if (name) {
+            this.items.push({
+                'name': name,
+                'comments': []
+            });
             localStorage.setItem('items', JSON.stringify(this.items));
-            console.log(comments);
+            this.filterName = '';
         }
     };
     AppItemsComponent.prototype.deleteItem = function (item) {
-        var deletedItem = this.items.indexOf(item);
-        this.items.splice(deletedItem, 1);
+        var delSensor = this.items.indexOf(item);
+        this.items.splice(delSensor, 1);
         localStorage.setItem('items', JSON.stringify(this.items));
     };
     AppItemsComponent.prototype.selectItem = function (item) {
-        this.getItems.saveItems(item);
-        this.isActive = !this.isActive;
+        item.isActive = !item.isActive;
+        this.getItems.setItem(item);
     };
     AppItemsComponent.prototype.ngOnInit = function () {
         if (localStorage.getItem('items') === null) {
             this.items = [];
         }
         else {
-            var names = localStorage.getItem('items');
-            var parsedData = JSON.parse(names);
-            this.items = parsedData;
+            var items = localStorage.getItem('items');
+            var parsedItems = JSON.parse(items);
+            this.items = parsedItems;
         }
+    };
+    AppItemsComponent.prototype.ngOnDestroy = function () {
     };
     AppItemsComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
@@ -217,7 +225,7 @@ var AppItemsComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".left {\n    background-color: #09083b;\n    color: white;\n    height: 650px;\n}\n\napp-app-items,\napp-app-comments {\n    margin-top: 1%;\n}"
+module.exports = ".left {\n    background-color: #3a1070;\n    color: white;\n    height: 650px;\n}\n\napp-app-items,\napp-app-comments {\n    margin-top: 1%;\n}"
 
 /***/ }),
 
@@ -252,7 +260,6 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.title = 'app';
     }
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
@@ -280,10 +287,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
 /* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
-/* harmony import */ var _app_items_app_items_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app-items/app-items.component */ "./src/app/app-items/app-items.component.ts");
-/* harmony import */ var _app_comments_app_comments_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app-comments/app-comments.component */ "./src/app/app-comments/app-comments.component.ts");
-/* harmony import */ var _get_items_service__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./get-items.service */ "./src/app/get-items.service.ts");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _app_items_app_items_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app-items/app-items.component */ "./src/app/app-items/app-items.component.ts");
+/* harmony import */ var _app_comments_app_comments_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./app-comments/app-comments.component */ "./src/app/app-comments/app-comments.component.ts");
+/* harmony import */ var _get_items_service__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./get-items.service */ "./src/app/get-items.service.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -296,21 +304,23 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"],
-                _app_items_app_items_component__WEBPACK_IMPORTED_MODULE_3__["AppItemsComponent"],
-                _app_comments_app_comments_component__WEBPACK_IMPORTED_MODULE_4__["AppCommentsComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"],
+                _app_items_app_items_component__WEBPACK_IMPORTED_MODULE_4__["AppItemsComponent"],
+                _app_comments_app_comments_component__WEBPACK_IMPORTED_MODULE_5__["AppCommentsComponent"]
             ],
             imports: [
-                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_2__["FormsModule"]
             ],
-            providers: [_get_items_service__WEBPACK_IMPORTED_MODULE_5__["GetItemsService"]],
-            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
+            providers: [_get_items_service__WEBPACK_IMPORTED_MODULE_6__["GetItemsService"]],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
@@ -345,13 +355,20 @@ var __metadata = (undefined && undefined.__metadata) || function (k, v) {
 
 var GetItemsService = /** @class */ (function () {
     function GetItemsService() {
-        this.state$ = new rxjs__WEBPACK_IMPORTED_MODULE_1__["BehaviorSubject"]('');
+        this.comment = new rxjs__WEBPACK_IMPORTED_MODULE_1__["Subject"]();
+        this.item = new rxjs__WEBPACK_IMPORTED_MODULE_1__["Subject"]();
     }
-    GetItemsService.prototype.saveItems = function (items) {
-        this.state$.next(items);
+    GetItemsService.prototype.getComment = function () {
+        return this.comment.asObservable();
     };
-    GetItemsService.prototype.getItems = function () {
-        return this.state$.asObservable();
+    GetItemsService.prototype.setComment = function (comment) {
+        return this.comment.next(comment);
+    };
+    GetItemsService.prototype.getItem = function () {
+        return this.item.asObservable();
+    };
+    GetItemsService.prototype.setItem = function (item) {
+        return this.item.next(item);
     };
     GetItemsService = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Injectable"])({
